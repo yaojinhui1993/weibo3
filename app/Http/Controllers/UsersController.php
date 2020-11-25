29b5b55a926fe33c0023030fea22c13f75e53a86
yaojinhui <yaojinhui1993@gmail.com>
2020-11-25 14:51:26 +0800

    Full login state

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UsersController extends Controller
 {
@@ -33,6 +34,7 @@ class UsersController extends Controller
             'password' => bcrypt($data['password']),
         ]);
 
+        Auth::login($user);
         session()->flash('success', '欢迎！您将开启一段新的旅程~');
         return redirect()->route('users.show', [$user]);
     }

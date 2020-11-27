b76acbec622187de7e12298acc121f5e2bf25d2c
yaojinhui <yaojinhui1993@gmail.com>
2020-11-27 09:46:49 +0800

    delete post

--- resources/views/statuses/_status.blade.php
+++ resources/views/statuses/_status.blade.php
@@ -8,5 +8,13 @@
       <small> / {{ $status->created_at->diffForHumans() }} </small>
     </h5>
     {{ $status->content }}
+
   </div>
+  @can('destroy', $status)
+    <form action="{{ route('statuses.destroy', $status->id) }}" method="post" onsubmit="return confirm('您确定要删除本条微博吗？');">
+      {{ csrf_field() }}
+      {{ method_field('DELETE') }}
+      <button class="btn btn-sm btn-danger" type="submit">删除</button>
+    </form>
+  @endcan
 </li>
